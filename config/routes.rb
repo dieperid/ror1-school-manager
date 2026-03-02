4e45800eef3521ab8c8f35fc2122d53f217b527e
ddieperi <david.dieperink@ik.me>
2026-03-02 13:38:35 +0100

    build(config): updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :account do
+    root to: "devise/sessions#new"
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -12,5 +16,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "home#index"
 end
