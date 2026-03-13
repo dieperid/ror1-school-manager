1cba4d026df9e9268fc93030a6e77ff7e82294c2
ddieperi <david.dieperink@ik.me>
2026-03-13 14:39:54 +0100

    feat: added possibility to add students to a school class

--- app/controllers/admin/school_classes_controller.rb
+++ app/controllers/admin/school_classes_controller.rb
@@ -6,10 +6,11 @@ module Admin
     before_action :ensure_prerequisites!, only: %i[new create]
 
     def index
-      @school_classes = SchoolClass.includes(:formation_plan, responsible_collaborator: [ :person, :collaborator_roles ]).order(:name)
+      @school_classes = SchoolClass.includes(:formation_plan, :students, responsible_collaborator: [ :person, :collaborator_roles ]).order(:name)
     end
 
     def show
+      @students = @school_class.students.joins(:person).includes(:person).order("people.last_name ASC, people.first_name ASC")
     end
 
     def new
@@ -18,7 +19,7 @@ module Admin
     def create
       @school_class.assign_attributes(school_class_params)
 
-      if @school_class.save
+      if save_school_class
         redirect_to admin_school_class_path(@school_class), notice: "School class created."
       else
         render :new, status: :unprocessable_entity
@@ -29,7 +30,9 @@ module Admin
     end
 
     def update
-      if @school_class.update(school_class_params)
+      @school_class.assign_attributes(school_class_params)
+
+      if save_school_class
         redirect_to admin_school_class_path(@school_class), notice: "School class updated."
       else
         render :edit, status: :unprocessable_entity
@@ -39,7 +42,7 @@ module Admin
     def destroy
       @school_class.destroy!
       redirect_to admin_school_classes_path, notice: "School class deleted."
-    rescue ActiveRecord::InvalidForeignKey
+    rescue ActiveRecord::DeleteRestrictionError, ActiveRecord::InvalidForeignKey
       redirect_to admin_school_class_path(@school_class), alert: "Delete the enrollments linked to this school class first."
     end
 
@@ -50,7 +53,7 @@ module Admin
     end
 
     def set_school_class
-      @school_class = SchoolClass.includes(:formation_plan, responsible_collaborator: [ :person, :collaborator_roles ]).find(params[:id])
+      @school_class = SchoolClass.includes(:formation_plan, :students, responsible_collaborator: [ :person, :collaborator_roles ]).find(params[:id])
     end
 
     def load_form_dependencies
@@ -58,6 +61,8 @@ module Admin
       @responsible_collaborators = Collaborator.joins(:person)
                                              .includes(:person, :collaborator_roles)
                                              .order("people.last_name ASC, people.first_name ASC")
+      @students = Student.joins(:person).includes(:person).order("people.last_name ASC, people.first_name ASC")
+      @selected_student_ids = selected_student_ids
     end
 
     def ensure_prerequisites!
@@ -73,6 +78,46 @@ module Admin
 
     def school_class_params
       params.fetch(:school_class, {}).permit(:name, :formation_plan_id, :responsible_collaborator_id)
+    end
+
+    def enrollment_params
+      params.fetch(:school_class, {}).permit(student_ids: [])
+    end
+
+    def selected_student_ids
+      ids =
+        if params[:school_class].present?
+          enrollment_params[:student_ids]
+        else
+          @school_class.student_ids
+        end
+
+      Array(ids).reject(&:blank?).map(&:to_i).uniq
+    end
+
+    def save_school_class
+      valid_class = @school_class.valid?
+      valid_students = selected_students_valid?
+
+      return false unless valid_class && valid_students
+
+      SchoolClass.transaction do
+        @school_class.save!
+        @school_class.student_ids = @selected_student_ids
+      end
+
+      true
+    rescue ActiveRecord::RecordInvalid
+      false
+    end
+
+    def selected_students_valid?
+      return true if @selected_student_ids.empty?
+
+      return true if Student.where(id: @selected_student_ids).count == @selected_student_ids.size
+
+      @school_class.errors.add(:base, "One or more selected students are invalid")
+      false
     end
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
   belongs_to :person
   belongs_to :departure_reason, optional: true
+  has_many :class_enrollments, dependent: :restrict_with_exception
+  has_many :school_classes, -> { order(:name) }, through: :class_enrollments
   has_many :grades, dependent: :restrict_with_exception
 
   validates :person_id, uniqueness: true
@@ -8,6 +10,10 @@ class Student < ApplicationRecord
   validate :student_dates_are_ordered
 
   delegate :full_name, to: :person
+
+  def school_class_names
+    school_classes.map(&:name)
+  end
 
   private
 
