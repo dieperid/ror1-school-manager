ed546ca6f40fb93e999ed48dab79095cee9d80cf
ddieperi <david.dieperink@ik.me>
2026-03-23 12:18:31 +0100

    feat: updated pagination to show "..." between pages so it doesn't show 20 pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 module ApplicationHelper
+  def pagination_page_items(pagination)
+    total_pages = pagination.total_pages
+    current_page = pagination.current_page
+
+    return (1..total_pages).to_a if total_pages <= 4
+
+    if current_page <= 2
+      [1, 2, 3, :gap, total_pages]
+    elsif current_page >= total_pages - 1
+      [1, :gap, total_pages - 1, total_pages]
+    else
+      [current_page - 1, current_page, current_page + 1, :gap, total_pages]
+    end
+  end
+
   def pagination_path_for(page_number)
     query = request.query_parameters.stringify_keys
 
